2b06ce42308cb7b5fc843faee629df2aa9ff4657
Marco de boer <76598975+Mdb1990@users.noreply.github.com>
2021-04-13 17:56:22 +0200

    Add files via upload

---
+++ Opdracht24JS/script.js
@@ -0,0 +1,19 @@
+/** @format */
+
+const makePizza = function () {
+  console.log("Make dough for pizza");
+  console.log("Lay some stuff on the pizza to make it good");
+  console.log("Bake the pizza 20min on 220 celcius");
+};
+
+const makeSushi = function () {
+  console.log("Boil rice");
+  console.log("Make the rolls with fish");
+  console.log("Serve the sushi with some wasabi");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
